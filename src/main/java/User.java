5050ab86ae506f9c77cc728c6fb3262444016b3b
Jeffrey Vavasour <jeffreyvavasour@gmail.com>
2023-12-12 15:38:14 +0000

    removed unnecessary methods from GardenCenterEmployee class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -19,38 +19,6 @@ public class User {
         return id;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-
     @Override
     public String toString() {
         return "User{" +
